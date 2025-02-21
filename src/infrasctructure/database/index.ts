593bfb9f4f3eeed5b30b94e72f403dbc2da6114f
fanecovsf <gustavo_h07@hotmail.com>
2025-02-20 21:08:18 -0300

    feat: adicionado middleware de hash do password

--- src/infrasctructure/database/index.ts
+++ src/infrasctructure/database/index.ts
@@ -1,8 +1,6 @@
 import { PrismaClient } from '@prisma/client';
 import { hashMiddleware } from './middlewares/hashMiddleware';
 
-const prismaClient = new PrismaClient();
-
-prismaClient.$extends(hashMiddleware);
+const prismaClient = new PrismaClient().$extends(hashMiddleware);
 
 export { prismaClient };
--- src/infrasctructure/database/middlewares/hashMiddleware.ts
+++ src/infrasctructure/database/middlewares/hashMiddleware.ts
@@ -3,15 +3,9 @@ import bcrypt from 'bcrypt';
 const hashMiddleware = {
   query: {
     user: {
-      async create({ args, query }: any) {
-        if (args.data?.password) {
-          args.data.password = await bcrypt.hash(args.data.password, 10);
-        }
-        return query(args);
-      },
-      async update({ args, query }: any) {
-        if (args.data?.password && typeof args.data.password === 'string') {
-          args.data.password = await bcrypt.hash(args.data.password, 10);
+      $allOperations({ operation, args, query }: any) {
+        if (['create', 'update'].includes(operation) && args.data['password']) {
+          args.data['password'] = bcrypt.hashSync(args.data['password'], 10);
         }
         return query(args);
       },
