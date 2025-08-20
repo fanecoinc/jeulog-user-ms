39c69f49f886e6d688039023a08a245a51e97e20
fanecovsf <gustavo_h07@hotmail.com>
2025-08-20 06:53:49 -0300

    feat: alterando setup de execução

--- src/tests/role.test.ts
+++ src/tests/role.test.ts
@@ -1,38 +1,7 @@
 import { Role } from '@/domain/entities/Role';
 import broker from '@/infrastructure/broker/service-broker';
-import { prismaClient } from '@/infrastructure/database';
-import fs from 'fs';
-import path from 'path';
-
-const testDbPath = path.resolve(
-  __dirname,
-  '../infrastructure/database/prisma/schema/test.db'
-);
-
-const testDbJournalPath = path.resolve(
-  __dirname,
-  '../infrastructure/database/prisma/schema/test.db-journal'
-);
 
 describe('Role Service E2E', () => {
-  beforeAll(async () => {
-    await prismaClient.$connect();
-    await broker.start();
-  });
-
-  afterAll(async () => {
-    await prismaClient.$disconnect();
-    await broker.stop();
-
-    if (fs.existsSync(testDbPath)) {
-      fs.unlinkSync(testDbPath);
-    }
-
-    if (fs.existsSync(testDbJournalPath)) {
-      fs.unlinkSync(testDbJournalPath);
-    }
-  });
-
   it('should create a role', async () => {
     const permissionIds = await broker
       .call('user.getPermissions')
